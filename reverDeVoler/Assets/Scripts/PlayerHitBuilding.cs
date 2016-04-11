da6e398eba0af6fd574d0dd45e12c072b623c730
timcampy77 julien <timcampy77 julien>
2016-04-11 18:31:12 +0200

    add collisions collectibles

---
+++ reverDeVoler/Assets/Scripts/PlayerHitBuilding.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollide : MonoBehaviour {
+
+    public Vector3 startPosition;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Building") {
+            transform.position = startPosition;
+        }
+        else {
+            transform.position = transform.position + Vector3.up * 10;
+        }
+    }
+}
