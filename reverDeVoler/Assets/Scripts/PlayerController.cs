7aefe0788add0077986440c61462362a9a19b0b9
julien Berthou - grifdail <julien@grifdail.fr>
2016-04-11 15:22:07 +0100

    add first prototype.

---
+++ reverDeVoler/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 1;
+    Transform head;
+
+	// Use this for initialization
+	void Start () {
+        head = transform.GetChild(0).GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += speed * head.forward * Time.deltaTime;
+	}
+}
