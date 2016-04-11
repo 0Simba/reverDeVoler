d951a772010c7f7d4f95e0d353a8702fd470ad22
timcampy77 julien <timcampy77 julien>
2016-04-11 19:13:46 +0200

    set smooth moving script

---
+++ reverDeVoler/Assets/Scripts/MovementLoop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementLoop : MonoBehaviour {
+
+    public AnimationCurve ratioTimeToRatioPosition;
+    private Vector3 initialPosition;
+    public Vector3 offSet;
+    public float duration;
+    
+    void Start ()
+    {
+        initialPosition = transform.position; 
+    }
+    
+    void Update ()
+    {
+        float ratioTime = (Time.time % duration) / duration;
+        float ratioPosition = ratioTimeToRatioPosition.Evaluate(ratioTime);
+        transform.position = Vector3.Lerp(initialPosition, initialPosition + offSet, ratioPosition);
+    } 
+}
