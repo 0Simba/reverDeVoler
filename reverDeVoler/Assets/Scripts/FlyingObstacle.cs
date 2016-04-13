74fefaf5f774423bc8f4053343f3f2c8e01a95b0
0Simba <b.dersoir@student.isartdigital.com>
2016-04-13 18:41:27 +0200

    Add flying obstacle script

---
+++ reverDeVoler/Assets/Scripts/FlyingObstacle.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class FlyingObstacle : Entity {
+
+
+    void Start () {
+        base.Init();
+
+        RandomRotate();
+        gameObject.tag = "Building"; 
+    }
+
+
+
+    void RandomRotate () {
+        float xRotation = 0;
+        float yRotation = Random.Range(-180, 180);
+        float zRotation = 0;
+
+        transform.Rotate(xRotation, yRotation, zRotation);
+    }
+}
