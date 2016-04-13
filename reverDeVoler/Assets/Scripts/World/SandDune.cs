542a7c02beed9f1cf1d694caf467a3f68d70c3f1
julien Berthou - grifdail <julien@grifdail.fr>
2016-04-13 19:24:45 +0100

    add new world gen

--- reverDeVoler/Assets/Scripts/World/SandDune.cs
+++ reverDeVoler/Assets/Scripts/World/SandDune.cs
@@ -10,7 +10,6 @@ public class SandDune : MonoBehaviour {
     void Start() {
         mesh = GetComponent<MeshFilter>().mesh;
         mesh.MarkDynamic();
-        
 	}
 	
 	// Update is called once per frame
