84147bd5a267c047e5c626cee0cc561dfa3f373f
julien Berthou - grifdail <julien@grifdail.fr>
2016-04-13 16:22:38 +0100

    sandFloor non infinie

---
+++ reverDeVoler/Assets/Scripts/World/SandDune.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SandDune : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            var p = transform.TransformPoint(vertices[i]);
+
+            vertices[i].z = Mathf.Sin(p.x*0.5f + p.z * 0.1f) * (1 + Mathf.PerlinNoise(p.x, p.z)) * 0.5f;
+            Debug.Log(vertices[i].y);
+        }
+        mesh.vertices = (vertices);
+        mesh.RecalculateNormals();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
