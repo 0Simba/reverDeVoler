794e82173c4d7d3d234f5c6185da44c267c88307
julien Berthou - grifdail <julien@grifdail.fr>
2016-04-14 19:37:45 +0100

    add rules page

---
+++ reverDeVoler/Assets/Scripts/EasyOndulation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyOndulation : MonoBehaviour {
+
+    public float scale = 1;
+    public float speed = 1;
+    public Vector3 dir = Vector3.up;
+    private Vector3 origin;
+
+	// Use this for initialization
+	void Start () {
+        origin = transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Time.time);
+        transform.localPosition = origin + Mathf.Cos(Time.realtimeSinceStartup * speed) * scale * dir;	    
+	}
+}
