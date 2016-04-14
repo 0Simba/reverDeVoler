7ac157a0308231480f75e226882e498d78a461fe
0Simba <b.dersoir@student.isartdigital.com>
2016-04-14 11:06:00 +0200

    Refactor target movement

--- reverDeVoler/Assets/Scripts/Target/TargetMovement.cs
+++ reverDeVoler/Assets/Scripts/Target/TargetMovement.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 
 public class TargetMovement : MonoBehaviour {
 
-    public List<Vector3> waypoints;
-    public Vector3       startOffset;
-    public float         speed        = 1;
-    public float         requiredDist = 1;
-    private Transform    startPoint;
+    public  List<Vector3> waypoints;
+    public  Vector3       startOffset;
+    public  float         speed        = 1;
+    public  float         requiredDist = 1;
+    public  float         playerDistanceMinimum;
+    public  float         playerDistanceMaximum;
+    private Transform     startPoint;
+    private Transform     player;
 
 
 
@@ -24,24 +27,32 @@ public class TargetMovement : MonoBehaviour {
 
 	
 	void Update () {
-        if (waypoints.Count > 0)
-        {
-            Vector3 nextPoint = waypoints[0];
-            Vector3 dir = (nextPoint - transform.position);
-            if (dir.sqrMagnitude > 1)
-            {
-                dir = dir.normalized;
-            }
-            transform.position += dir * Time.deltaTime * speed;
-            if ((nextPoint - transform.position).magnitude < requiredDist)
-            {
-                waypoints.RemoveAt(0);
-                if (waypoints.Count == 0)
-                {
-                    waypoints.Add(Random.insideUnitSphere * 50 + Vector3.forward * 25 + Vector3.up * 30);
-                }
-            }
+        if (waypoints.Count == 0) {
+            return;
         }
-        
+
+
+        Vector3 nextPoint = waypoints[0];
+        Vector3 direction = (nextPoint - transform.position);
+
+        MoveFromDirection(direction.normalized);
+
+        if ((nextPoint - transform.position).magnitude < requiredDist) {
+            SetNextWayPoint();
+        }
 	}
+
+
+    void MoveFromDirection (Vector3 direction) {
+        transform.position += direction * Time.deltaTime * speed;
+    }
+
+
+    void SetNextWayPoint () {
+        waypoints.RemoveAt(0);
+
+        if (waypoints.Count == 0) {
+            waypoints.Add(Random.insideUnitSphere * 50 + Vector3.forward * 25 + Vector3.up * 30);
+        }        
+    }
 }
